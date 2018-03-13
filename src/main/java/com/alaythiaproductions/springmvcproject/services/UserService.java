287fce58548575a468655fbdbe3ea5703979c5d2
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2018-03-13 10:59:09 -0500

    Spring 2.0.0 Release
    JpaRepository not working with findOne or findById. Reverting back to Spring 1.5.9

---
+++ src/main/java/com/alaythiaproductions/springmvcproject/services/UserService.java
@@ -0,0 +1,42 @@
+package com.alaythiaproductions.springmvcproject.services;
+
+import com.alaythiaproductions.springmvcproject.models.Role;
+import com.alaythiaproductions.springmvcproject.models.User;
+import com.alaythiaproductions.springmvcproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUser(User user) {
+        BCryptPasswordEncoder encorder = new BCryptPasswordEncoder();
+        user.setPassword(encorder.encode(user.getPassword()));
+        Role userRole = new Role("USER");
+        List<Role> roles = new ArrayList<>();
+        roles.add(userRole);
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+    public void createAdmin(User user) {
+        BCryptPasswordEncoder encorder = new BCryptPasswordEncoder();
+        user.setPassword(encorder.encode(user.getPassword()));
+        Role userRole = new Role("ADMIN");
+        List<Role> roles = new ArrayList<>();
+        roles.add(userRole);
+        user.setRoles(roles);
+        userRepository.save(user);
+    }
+
+    public User findOne(String email) {
+        return null;
+    }
+}
